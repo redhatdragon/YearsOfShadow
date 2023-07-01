d4bd462b9a43a550d2cd668fe01dd5b227a19d44
redhatdragon <paulabrams99@yahoo.com>
2023-07-01 03:25:34 -0400

    Set NPC count back to 10k
    SystemVoxel takes about 3 seconds to initialize now, should be viable to test with this size

--- YearsOfShadow/YearsOfShadow/Systems/Systems.h
+++ YearsOfShadow/YearsOfShadow/Systems/Systems.h
@@ -13,8 +13,8 @@ HAL::thread_pool_handle_t threadPool;
 //#include <stddef>
 
 constexpr uint32_t chunk_width = 16, chunk_depth = 16, chunk_height = 256;
-constexpr uint32_t world_size = 240 - (240 % chunk_width);  //block across
-constexpr uint32_t max_npc = 100;
+constexpr uint32_t world_size = 400 - (400 % chunk_width);  //block across
+constexpr uint32_t max_npc = 10000;
 //constexpr uint32_t hash_width = 2;
 //constexpr uint32_t max_bodies_per_hash = 16;
 constexpr uint32_t hash_width = 1;
