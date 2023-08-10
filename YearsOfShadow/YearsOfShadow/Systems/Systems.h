7b7190291804c75ef0641f8580a6d62ea3ba1c3c
redhatdragon <paulabrams99@yahoo.com>
2023-08-10 04:56:35 -0400

    Bumped NPC count

--- YearsOfShadow/YearsOfShadow/Systems/Systems.h
+++ YearsOfShadow/YearsOfShadow/Systems/Systems.h
@@ -13,8 +13,8 @@ HAL::thread_pool_handle_t threadPool;
 //#include <stddef>
 
 constexpr uint32_t chunk_width = 16, chunk_depth = 16, chunk_height = 256;
-constexpr uint32_t world_size = 200 - (200 % chunk_width);  //block across
-constexpr uint32_t max_npc = 25;
+constexpr uint32_t world_size = 480 - (480 % chunk_width);  //block across
+constexpr uint32_t max_npc = 10000;
 //constexpr uint32_t hash_width = 2;
 //constexpr uint32_t max_bodies_per_hash = 16;
 constexpr uint32_t hash_width = 1;
