66d376dab828765555d537f63f4e2db904572935
redhatdragon <paulabrams99@yahoo.com>
2023-08-12 04:28:11 -0400

    Cleanup app.cpp

--- YearsOfShadow/YearsOfShadow/app.cpp
+++ YearsOfShadow/YearsOfShadow/app.cpp
@@ -2,7 +2,6 @@
 #include "Systems/Systems.h"
 #include <thread>
 #include <optick.h>
-//#include <EchoEngine/OP.h>
 #include <Windows.h>
 
 static NTSTATUS(__stdcall *NtDelayExecution)(BOOL Alertable, PLARGE_INTEGER DelayInterval) =
@@ -37,14 +36,9 @@ void sleepForThreaded(void* data) {
 
 
 
-void* exampleMesh;
-void* exampleInstancedMeshTest;
 void HAL::app_start() {
     OPTICK_START_CAPTURE();
     OPTICK_THREAD("MainThread");
-
-    //OP_THREAD("MainThread");
-    //OP_START();
     initSystems();
     physics.setRecording(true);
 }
@@ -52,20 +46,8 @@ bool FPressedLastTick = true;
 void HAL::app_loop() {
     OPTICK_THREAD("MainThread");
     OPTICK_EVENT();
-    //OP_CAP_FUNCTION();
     HAL::draw_background(0, 0, 0, 255);
     ecs.runSystems();
-    // auto dbgInfo = ecs.getDebugInfoStr();
-    // for (auto& i : dbgInfo)
-    //	std::cout << i << std::endl;
-    // auto dbgInfo = ecs.getDebugInfoStr();
-    // std::string dbgStr = "";
-    // for (auto &str : dbgInfo)
-    // {
-    //     dbgStr += str;
-    //     dbgStr += '\n';
-    // }
-    // fwrite(dbgStr.c_str(), dbgStr.size(), 1, stderr);
 
 #ifdef REWIND_ENABLED
     static int ticksSinceRewind = 0;
@@ -91,14 +73,6 @@ void HAL::app_post_frame() {
     u16 threadCount = static_cast<u16>(HAL::get_thread_pool_free_thread_count(threadPool));
     clock_t remainingTime = static_cast<clock_t>(HAL::get_app_loop_time_MS() + HAL::get_draw_time_MS());
     i32 waitFor = 16 - remainingTime;
-    // std::string printStr = "waitFor: ";
-    // printStr += std::to_string(waitFor);
-    // printStr += " LoopTime: ";
-    // printStr += std::to_string(HAL::get_app_loop_time_MS());
-    // printStr += " DrawTime: ";
-    // printStr += std::to_string(HAL::get_draw_time_MS());
-    // printStr += '\n';
-    // fwrite(printStr.c_str(), printStr.size(), 1, stderr);
     if (waitFor <= 4)
         return;
     for (u32 i = 0; i < threadCount; i++)
