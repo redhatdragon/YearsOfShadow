22b078ef00f40f635e351a642c94831f58f2d3d7
redhatdragon <paulabrams99@yahoo.com>
2023-10-01 03:43:18 -0400

    Unfinished update to NetworkingTools.h

--- YearsOfShadow/YearsOfShadow/Systems/SystemUtilities/NetworkingTools.h
+++ YearsOfShadow/YearsOfShadow/Systems/SystemUtilities/NetworkingTools.h
@@ -1,39 +1,73 @@
-struct PacketDataHeader {
-	static constexpr uint16_t PACKET_MAX_LEN = 65000;
-	uint64_t messageID;
-	uint16_t packetNum;
-	uint8_t padding[6];
-	uint8_t data[PACKET_MAX_LEN];
-};
+#include <ranges>
 
-struct Packet {
-	uint8_t* data;
+constexpr uint16_t PACKET_MAX_LEN = 65000;
+
+class Packet {
+	struct PacketData {
+		uint64_t messageID;
+		uint16_t packetNum;
+		uint8_t toClient;  //0 == true, else toServer
+		uint8_t confirmedSent;
+		uint16_t packetCount;
+		uint8_t padding[2];
+		uint8_t data[PACKET_MAX_LEN];
+	} data;
 	uint16_t len;
+public:
+	void write(const uint8_t* _data, uint16_t _len,
+		uint64_t _messageID, uint16_t _packetNum, uint8_t _toClient, uint16_t _packetCount) {
+		memcpy(data.data, _data, _len);
+		len = _len;
+		data.messageID = _messageID;
+		data.packetNum = _packetNum;
+		data.toClient = _toClient;
+		data.confirmedSent = false;
+		data.packetCount = _packetCount;
+	}
+	void send() {
+
+	}
+	void confirmToSender() {
+
+	}
+	bool isSent() {
+		return (bool)data.confirmedSent;
+	}
 };
 
 struct PacketPool {
-	FlatFlaggedBuffer<Packet, 1000> packets;
-};
+	static constexpr uint32_t max_packets = 10000;
+	Packet packets[max_packets];
+	FlatBuffer<Packet*, max_packets> packetPtrs;
+	PacketPool() {
+		packetPtrs.clear();
+		for (uint32_t i = 0; i < max_packets; i++) {
+			packetPtrs[i] = &packets[i];
+		}
+	}
+	Packet* get() {
+		packetPtrs.pop();
+		Packet* retValue = packetPtrs[packetPtrs.count];
+		return retValue;
+	}
+	void ret(Packet* packet) {
+		packetPtrs.push(packet);
+	}
+} packetPool = PacketPool();
 
 class NetworkMessage {
 	std::string ip;
-	uint8_t* msg;
-	uint32_t size;
 	uint64_t id;
 	static uint64_t nextID;
-	std::vector<Packet*> packets;
+	FlatBuffer<Packet*, PacketPool::max_packets> packets;
 public:
-	void init(const char* _ip, uint8_t* _msg, uint32_t _size) {
-		ip = _ip;
-		msg = _msg;
-		size = _size;
-		id = nextID;
-		nextID++;
-		uint16_t totalPackets = _size / PacketDataHeader::PACKET_MAX_LEN;
-		uint16_t remainder = _size % PacketDataHeader::PACKET_MAX_LEN;
+	NetworkMessage(std::string_view _ip, std::span<const uint8_t> _msg)
+	: ip(_ip), id(nextID++) {
+		uint16_t totalPackets = _msg.size() / PACKET_MAX_LEN;
+		uint16_t remainder = _msg.size() % PACKET_MAX_LEN;
 		for (uint32_t i = 0; i < totalPackets; i++)
-			pushNewPacket(&_msg[i * PacketDataHeader::PACKET_MAX_LEN], PacketDataHeader::PACKET_MAX_LEN);
-		pushNewPacket(&_msg[totalPackets * PacketDataHeader::PACKET_MAX_LEN], remainder);
+			pushNewPacket(&_msg[i * PACKET_MAX_LEN], PACKET_MAX_LEN);
+		pushNewPacket(&_msg[totalPackets * PACKET_MAX_LEN], remainder);
 	}
 	void send() {
 
@@ -41,15 +75,11 @@ public:
 	bool isFinished() {
 
 	}
-	void clean() {
-		ip = "";
-		free(msg);
-		msg = nullptr;
-		size = 0;
-	}
 private:
 	void pushNewPacket(const uint8_t* data, uint16_t size) {
-
+		Packet* packet = packetPool.get();
+		packet->write(data, size);
+		packets.push(packet);
 	}
 };
 uint64_t NetworkMessage::nextID = 0;
@@ -62,22 +92,27 @@ public:
 	void init(uint16_t _inPort, uint16_t _outPort) {
 		inPort = _inPort;
 		outPort = _outPort;
+		messages.reserve(10);
 	}
-	void push(const char* ip, const uint8_t* data, uint32_t size) {
-		uint8_t* msg = (uint8_t*)malloc(size);
-		if (msg == nullptr) {
-			HAL_ERROR("NetworkStream::send() can't allocate enough space for msg!");
-			throw;
-		}
-		memcpy(msg, data, size);
-		NetworkMessage nm;
-		nm.init(ip, msg, size);
-		messages.push_back(nm);
+	void push(std::string_view ip, const uint8_t* data, uint32_t size) {
+		//uint8_t* msg = (uint8_t*)malloc(size);
+		//if (msg == nullptr) {
+		//	HAL_ERROR("NetworkStream::send() can't allocate enough space for msg!");
+		//	throw;
+		//}
+		//memcpy(msg, data, size);
+		messages.push_back(NetworkMessage(ip, {data, size} ));
+		//messages[messages.size() - 1].init(ip, msg, size);
 	}
 	void sendAll() {
 		uint32_t count = (uint32_t)messages.size();
 		for (uint32_t i = 0; i < count; i++) {
 			messages[i].send();
 		}
+	}
+	void clear() {
+		conn = HAL::invalid_udp_socket_handle;
+		inPort = outPort = 0;
+		messages.resize(0);
 	}
 };
