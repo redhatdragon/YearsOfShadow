b3743dc9cd6ecb574874f31f8d37cc98fb18ee29
redhatdragon <paulabrams99@yahoo.com>
2023-10-13 00:29:36 -0400

    Minor refactor of FlatBuffer

--- EchoEngine/EchoEngine/FlatBuffer.h
+++ EchoEngine/EchoEngine/FlatBuffer.h
@@ -309,9 +309,9 @@ class FlatBuffer {
 	T data[maxLength];
 public:
 	uint32_t count = 0;
-	__forceinline void operator=(const FlatBuffer& other) {
+	void copyIntoShallow(const FlatBuffer& other) {
 		memcpy(data, other.data, other.count * sizeof(T));
-        count = other.count;
+		count = other.count;
 	}
 	__forceinline T& operator[](uint32_t index) {
 		return data[index];
