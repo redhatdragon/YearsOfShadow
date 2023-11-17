0473fd7a20550946e028094f754094785a3f203c
redhatdragon <paulabrams99@yahoo.com>
2023-11-16 23:54:48 -0500

    Another unfinished net update

--- YearsOfShadow/YearsOfShadow/Systems/SystemUtilities/NetworkingTools.h
+++ YearsOfShadow/YearsOfShadow/Systems/SystemUtilities/NetworkingTools.h
@@ -42,7 +42,7 @@ namespace NetworkingUtilities {
 
 	struct Packet {
 		// Though much data is duplicated between packets, it makes things easier/safer.
-		uint16_t len;
+		uint16_t dataLen;
 		uint16_t packetNum;
 		uint16_t packetCount;
 		enum OPERATION_STATE : uint8_t {
@@ -60,23 +60,26 @@ namespace NetworkingUtilities {
 		void write(const uint8_t* _data, uint16_t _len,
 			uint64_t _messageID, uint16_t _packetNum, uint16_t _packetCount) {
 			memcpy(data, _data, _len);
-			len = _len;
-			memcpy(signature, "YoS Pack", sizeof("YoS Pack"));
+			dataLen = _len;
+			memcpy(signature, "YoS Pack", 8);
 			messageID = _messageID;
 			packetNum = _packetNum;
 			operationState = INITIAL_SEND;
 			packetCount = _packetCount;
 		}
 		inline void cpyFrom(const Packet& other) {
-			memcpy(this, &other, other.len + getHeaderSize());
+			memcpy(this, &other, other.dataLen + getHeaderSize());
 		}
 		inline bool isSignatureValid() const {
-			if (strcmp(signature, "YoS Pack") == 0)
+			if (strncmp(signature, "YoS Pack", 8) == 0)
 				return true;
 			return false;
 		}
 		inline static uint8_t getHeaderSize() {
 			return 8 + 8 + 8;
+		}
+		inline void sendTo(HAL::udp_socket_handle_t conn, std::string_view ip) {
+			HAL::UDP_send_packet(conn, (uint8_t*)this, getHeaderSize() + dataLen, ip.data());
 		}
 	};
 
@@ -84,10 +87,10 @@ namespace NetworkingUtilities {
 		Packet packets[PACKETS_MAX];
 		FlatBuffer<Packet*, PACKETS_MAX> packetPtrs;
 		void setup() {
-			packetPtrs.clear();
 			for (uint32_t i = 0; i < PACKETS_MAX; i++) {
 				packetPtrs[i] = &packets[i];
 			}
+			packetPtrs.count = PACKETS_MAX;
 		}
 		Packet* get() {
 			packetPtrs.pop();
@@ -107,19 +110,12 @@ namespace NetworkingUtilities {
 		PacketPool* packetPoolPtr;
 
 		//Total data passed must be less than PACKETS_MAX * PACKET_MAX_LEN
-		bool tryConstruct(std::span<const uint8_t> _msg, PacketPool& _packetPool) {
+		bool tryConstructForSend(std::span<const uint8_t> _msg, PacketPool& _packetPool) {
+			//TODO: consider getting rid of try prefix, shoudl work anyway?
 			packetPoolPtr = &_packetPool;
 			packets.count = 0;
 			uint16_t totalFullPackets = (uint16_t)(_msg.size() / PACKET_MAX_LEN);
 			uint16_t remainder = _msg.size() % PACKET_MAX_LEN;
-			if (packets.count) {
-				HAL_ERROR("NetworkMessage::construct() called from non destroyed !");
-				throw;
-			}
-			if (canConstruct(_msg) == false) {
-				HAL_WARN("NetworkMessage::construct() failed to construct with input data!");
-				return false;
-			}
 			messageID = nextMessageID++;
 			if (remainder) {
 				for (uint32_t i = 0; i < totalFullPackets; i++)
@@ -130,6 +126,52 @@ namespace NetworkingUtilities {
 				for (uint32_t i = 0; i < totalFullPackets; i++)
 					pushNewPacket(&_msg[i * PACKET_MAX_LEN], PACKET_MAX_LEN, i, totalFullPackets);
 			}
+
+
+			return true;
+		}
+		bool tryConstructForRecv(const uint8_t* _data, uint16_t len, PacketPool& _packetPool) {
+			if (len > PACKET_MAX_LEN) {
+				HAL_WARN("NetworkMessage::tryConstructForRecv()\n");
+				HAL_WARN("input len: {} >= PACKET_MAX_LEN: {}",
+					len, PACKET_MAX_LEN);
+				return false;
+			}
+			if (len <= Packet::getHeaderSize()) {
+				HAL_WARN("NetworkMessage::tryConstructForRecv()\n");
+				HAL_WARN("input len: {} <= packetHeaderSize(): {}\n",
+					len, Packet::getHeaderSize());
+				return false;
+			}
+			const Packet* packet = (const Packet*)_data;
+			if (packet->isSignatureValid() == false) {
+				HAL_WARN("NetworkMessage::tryConstructForRecv()\n");
+				HAL_WARN("input packet's signature is invalid\n");
+				char buff[9] = { 0 };
+				memcpy(buff, packet->signature, 8);
+				HAL_WARN("Expected 'Yos Pack' got '{}'\n", buff);
+				return false;
+			}
+			if (packet->packetNum >= packet->packetCount) {
+				HAL_WARN("NetworkMessage::tryConstructForRecv()\n");
+				HAL_WARN("input packet's packetNum >= input packet's packetCount\n");
+				return false;
+			}
+			if (packet->dataLen > len - Packet::getHeaderSize()) {
+				HAL_WARN("NetworkMessage::tryConstructForRecv()\n");
+				HAL_WARN("input packet's specified length > actual capacity\n");
+				return false;
+			}
+			if (packet->operationState != Packet::OPERATION_STATE::INITIAL_SEND) {
+				HAL_WARN("NetworkMessage::tryConstructForRecv()\n");
+				HAL_WARN("input packet's operation state != INITIAL_SEND\n");
+				return false;
+			}
+			packetPoolPtr = &_packetPool;
+			Packet* newPacket = packetPoolPtr->get();
+			newPacket->cpyFrom(*packet);
+			packets.clear();
+			packets.push(newPacket);
 			return true;
 		}
 		void deconstruct() {
@@ -141,9 +183,39 @@ namespace NetworkingUtilities {
 		}
 		//Return true if packet belongs to this NetworkMessage.
 		bool tryUpdate(const uint8_t* _packet, uint16_t len) {
-			const Packet* inPacketPtr = (const Packet*)_packet;
-			if (isPacketValid(_packet, len) == false)
+			if (len > PACKET_MAX_LEN) {
+				HAL_WARN("NetworkMessage::tryUpdate()\n");
+				HAL_WARN("NetworkMessage::tryUpdate()'s input len: {} >= PACKET_MAX_LEN: {}\n",
+					len, PACKET_MAX_LEN);
 				return false;
+			}
+			if (len <= Packet::getHeaderSize()) {
+				HAL_WARN("NetworkMessage::tryUpdate()\n");
+				HAL_WARN("input len: {} <= packetHeaderSize(): {}\n",
+					len, Packet::getHeaderSize());
+				return false;
+			}
+			const Packet* inPacketPtr = (const Packet*)_packet;
+			if (inPacketPtr->messageID != getMessageID())
+				return false;
+			if (inPacketPtr->isSignatureValid() == false) {
+				HAL_WARN("NetworkMessage::tryUpdate()\n");
+				HAL_WARN("input packet's signature is invalid\n");
+				char buff[9] = { 0 };
+				memcpy(buff, inPacketPtr->signature, 8);
+				HAL_WARN("Expected 'Yos Pack' got '{}'\n", buff);
+				return false;
+			}
+			if (inPacketPtr->packetNum >= getPacketCount()) {
+				HAL_WARN("NetworkMessage::tryUpdate()\n");
+				HAL_WARN("input packet's packetNum >= packetCount\n");
+				return false;
+			}
+			if (inPacketPtr->packetCount != getPacketCount()) {
+				HAL_WARN("NetworkMessage::tryUpdate()\n");
+				HAL_WARN("input packet's packetCount doesn't match\n");
+				return false;
+			}
 			if (inPacketPtr->operationState == Packet::INITIAL_SEND) {
 				Packet* packet = packets[inPacketPtr->packetNum];
 				if (packet == nullptr)
@@ -159,7 +231,7 @@ namespace NetworkingUtilities {
 				uint32_t count = packets.count;
 				out.reserve(count * PACKET_MAX_LEN);
 				for (uint32_t i = 0; i < count; i++) {
-					uint32_t packLen = packets[i]->len;
+					uint32_t packLen = packets[i]->dataLen;
 					for (uint32_t j = 0; j < packLen; j++)
 						out.push_back(packets[i]->data[j]);
 					packetPoolPtr->ret(packets[i]);
@@ -182,50 +254,6 @@ namespace NetworkingUtilities {
 			packets.push(packet);
 		}
 
-		bool canConstruct(std::span<const uint8_t> _msg) {
-			const Packet* packetPtr = (const Packet*)_msg.data();
-			if (_msg.size() <= packetPtr->getHeaderSize()) {
-				HAL_WARN("input packet's physical length <= expected header size");
-				return false;
-			}
-			if (packetPtr->isSignatureValid() == false) {
-				HAL_WARN("input packet's signature is invalid");
-				return false;
-			}
-			if (packetPtr->packetNum >= packetPtr->packetCount) {
-				HAL_WARN("input packet's packetNum >= input packet's packetCount");
-				return false;
-			}
-			if (packetPtr->len > _msg.size() - Packet::getHeaderSize()) {
-				HAL_WARN("input packet's specified length > actual capacity");
-				return false;
-			}
-			if (packetPtr->operationState != Packet::OPERATION_STATE::INITIAL_SEND) {
-				HAL_WARN("input packet's operation state != INITIAL_SEND");
-				return false;
-			}
-			return true;
-		}
-
-		bool isPacketValid(const uint8_t* _packetPtr, uint16_t len) {
-			const Packet* packetPtr = (const Packet*)_packetPtr;
-			if (packetPtr->messageID != getMessageID())
-				return false;
-			if (packetPtr->isSignatureValid() == false) {
-				HAL_WARN("input packet's signature is invalid");
-				return false;
-			}
-			if (packetPtr->packetNum >= getPacketCount()) {
-				HAL_WARN("input packet's packetNum >= packetCount");
-				return false;
-			}
-			if (packetPtr->packetCount != getPacketCount()) {
-				HAL_WARN("input packet's packetCount doesn't match");
-				return false;
-			}
-			return true;
-		}
-
 		uint64_t getMessageID() {
 			Packet* packet = getFirstValidPacket();
 			return packet->messageID;
@@ -241,6 +269,7 @@ namespace NetworkingUtilities {
 				if (packets[i])
 					return packets[i];
 			HAL_PANIC("NetworkMessage::getFirstPacket() can't find valid packet?!");
+			return nullptr;
 		}
 	};
 	uint64_t NetworkMessage::nextMessageID = 0;
@@ -261,8 +290,10 @@ namespace NetworkingUtilities {
 			ourOutPort = HAL::UDP_get_our_port(conn);
 			recvMsgs.reserve(CONNECTIONS_MAX);
 			sentMsgs.reserve(CONNECTIONS_MAX);
+
+			packetPool.setup();
 		}
-		void sendTo(std::string_view ip, std::span<uint8_t> msg) {
+		void trySendTo(std::string_view ip, const uint8_t* msg, uint32_t len) {
 			FlatBuffer<NetworkMessage, MESSAGES_PER_CONNECTION_MAX>* msgsPtr;
 			if (sentMsgs.find(ip.data()) == sentMsgs.end()) {
 				sentMsgs[ip.data()] = {};
@@ -271,44 +302,21 @@ namespace NetworkingUtilities {
 			} else
 				msgsPtr = &sentMsgs[ip.data()];
 			FlatBuffer<NetworkMessage, MESSAGES_PER_CONNECTION_MAX>& msgs = *msgsPtr;
-			NetworkMessage& nm = msgs.push({});
-			nm.tryConstruct(msg, packetPool);
-		}
-		//Call once per game tick
-		void update() {
-			//for (auto& msgs : sentMsgs) {
-			//	uint32_t count = msgs.second.count;
-			//	for (uint32_t i = 0; i < count; i++) {
-			//		NetworkMessage& msg = msgs.second[i];
-			//		msg.update();
-			//		if (msg.isFinished())
-			//			msg.deconstruct();
-			//	}
-			//}
-			//for (auto& msgs : recvMsgs) {
-			//	uint32_t count = msgs.second.count;
-			//	for (uint32_t i = 0; i < count; i++) {
-			//		NetworkMessage& msg = msgs.second[i];
-			//		msg.update();
-			//	}
-			//}
-
-			static FlatBuffer<uint8_t, 256 * 256> buff;
-			std::string senderIP;
-			uint16_t outPort;  //TODO: still need to verify port is proper...
-			while (true) {
-				HAL::UDP_get_packet(conn, &buff[0], buff.count, senderIP, outPort);
-				if (buff.count == 0)
-					break;
-
-				processPacket(&buff[0], buff.count, senderIP, outPort);
+			if (msgs.isFull()) {
+				HAL_WARN("NetworkManager::sendTo() failed to send, overloaded with messages to same ip\n");
+				HAL_WARN("IP: {}; MSG_LEN: {}\n", ip, len);
+				return;
 			}
+			NetworkMessage& nm = msgs.push({});
+			nm.tryConstructForSend({msg, len}, packetPool);
+			sendTo(nm, ip);
 		}
 		void clear() {
 			sentMsgs.clear();
 		}
 
 		bool tryPopNextMsg(std::vector<uint8_t>& out) {
+			update();
 			for (auto& e : recvMsgs) {
 				for (uint32_t i = 0; i < e.second.count; i++) {
 					NetworkMessage& nm = e.second[i];
@@ -321,6 +329,20 @@ namespace NetworkingUtilities {
 			return false;
 		}
 	private:
+		void update() {
+			static FlatBuffer<uint8_t, 256 * 256> buff;
+			std::string senderIP;
+			uint16_t outPort;  //TODO: still need to verify port is proper...
+			while (true) {
+				buff.count = 256 * 256;  //tell max length
+				HAL::UDP_get_packet(conn, &buff[0], buff.count, senderIP, outPort);
+				if (buff.count == 0)
+					break;
+
+				processPacket(&buff[0], buff.count, senderIP, outPort);
+			}
+		}
+
 		void processPacket(const uint8_t* data, uint16_t len, std::string_view senderIP, uint16_t senderPort) {
 			const char* ipStr = senderIP.data();
 			if (recvMsgs.find(ipStr) == recvMsgs.end()) {
@@ -336,59 +358,18 @@ namespace NetworkingUtilities {
 				}
 			}
 			NetworkMessage nm;
-			if(nm.tryConstruct({data, len}, packetPool) == false)
-				HAL_WARN("Invalid packet came in");
+			nm.tryConstructForRecv(data, len, packetPool);
+			//if(nm.tryConstruct({data, len}, packetPool) == false)
+			//	HAL_WARN("Invalid packet came in");
+		}
+
+		void sendTo(NetworkMessage& nm, std::string_view ip) {
+			uint32_t count = nm.packets.count;
+			for (uint32_t i = 0; i < count; i++)
+				nm.packets[i]->sendTo(conn, ip);
 		}
 	};
 	constexpr uint64_t sizeofNetworkManager_MB_ish =
 		((sizeof(NetworkMessage) * MESSAGES_PER_CONNECTION_MAX * CONNECTIONS_MAX * 2) / 1000000)
 		+ sizeof(PacketPool) / 1000000;
-
-	//Might get renamed/removed soon...
-	//Meant to house all messages between one client
-	//class NetworkStream {
-	//	//HAL::udp_socket_handle_t conn;
-	//	std::string outIP;
-	//	uint16_t inPort, outPort;
-	//	std::vector<NetworkMessage> messages;
-	//public:
-	//	void init(std::string_view _outIP, uint16_t _inPort, uint16_t _outPort) {
-	//		outIP = _outIP;
-	//		inPort = _inPort;
-	//		outPort = _outPort;
-	//		messages.reserve(10);
-	//	}
-	//	void push(std::string_view ip, const uint8_t* data, uint32_t size) {
-	//		//uint8_t* msg = (uint8_t*)malloc(size);
-	//		//if (msg == nullptr) {
-	//		//	HAL_ERROR("NetworkStream::send() can't allocate enough space for msg!");
-	//		//	throw;
-	//		//}
-	//		//memcpy(msg, data, size);
-	//		//messages.push_back(NetworkMessage(ip, {data, size} ));
-	//		messages.push_back({});
-	//		messages[messages.size() - 1].init(ip, { data, size });
-	//	}
-	//	void sendAll() {
-	//		uint32_t count = (uint32_t)messages.size();
-	//		for (uint32_t i = 0; i < count; i++) {
-	//			messages[i].send();
-	//		}
-	//	}
-	//	void clear() {
-	//		outIP = "";
-	//		inPort = outPort = 0;
-	//		messages.resize(0);
-	//	}
-	//	bool consumePacketIfMatching(const uint8_t* packet, std::string_view _outIP) {
-	//		if (outIP != _outIP)
-	//			return false;
-	//		consume(packet);
-	//		return true;
-	//	}
-	//private:
-	//	void consume(const uint8_t* packet) {
-	//
-	//	}
-	//};
 }
