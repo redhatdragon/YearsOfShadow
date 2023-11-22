f61f740221f245390a4a5cd646b682732f59e819
redhatdragon <paulabrams99@yahoo.com>
2023-11-22 06:46:13 -0500

    Unfinished Serialize update

--- YearsOfShadow/YearsOfShadow/Systems/SystemUtilities/Serialize.h
+++ YearsOfShadow/YearsOfShadow/Systems/SystemUtilities/Serialize.h
@@ -3,25 +3,49 @@
 #include <unordered_map>
 
 namespace SystemUtilities {
+	//A purely blittable object
+	//Freeing directly should thus be safe
 	class SerialEntity {
 		struct Component {
 			Name name;
 			uint32_t size;
 			uint8_t data[1];  //Not allowed to do 0 length array gaaaaaaaaaaaay
+			static constexpr uint32_t getHeaderSize() {
+				return sizeof(name) + sizeof(size);
+			}
 		};
 		uint8_t components[1];
 		Component* getRootComponent() {
 			return (Component*)components;
 		}
 	public:
-		void construct(EntityID entity) {
+		static SerialEntity* constructNew(EntityID entity) {
 			static FlatBuffer<ComponentID, 32> idBuff;
 			idBuff.clear();
 			ecs.getEntityComponentIDs(entity, idBuff);
 			uint32_t idCount = idBuff.count;
+			uint32_t totalSize = 0;
 			for (uint32_t i = 0; i < idCount; i++) {
-				uint32_t componentSize = ecs.getComponentSize(idBuff[i]);
+				if (ecs.isComponentTypeDArray(idBuff[i])) {
 
+					continue;
+				}
+				totalSize += ecs.getComponentSize(idBuff[i]);
+			}
+			totalSize += Component::getHeaderSize() * idCount;
+			SerialEntity* retValue = (SerialEntity*)malloc(totalSize);
+			if (retValue == nullptr)
+				HAL_PANIC("SerialEntity::constructNew() failed to allocate new SerialEntity with size {}\n", totalSize);
+			uint8_t* retValueOffset = (uint8_t*)retValue;
+			for (uint32_t i = 0; i < idCount; i++) {
+				if (ecs.isComponentTypeDArray(idBuff[i])) {
+
+					continue;
+				}
+				uint32_t componentSize = ecs.getComponentSize(idBuff[i]);
+				void* componentData = ecs.getEntityComponent(entity, idBuff[i]);
+				memcpy(retValueOffset, componentData, componentSize);
+				retValueOffset += componentSize;
 			}
 		}
 	};
