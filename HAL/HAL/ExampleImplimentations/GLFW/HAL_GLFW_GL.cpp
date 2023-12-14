8a6b07f322a50d7a9d2c4147dcfe1b744ff7849c
redhatdragon <paulabrams99@yahoo.com>
2023-12-14 09:19:20 -0500

    textured quads now draw in right pos,siz

--- HAL/HAL/ExampleImplimentations/GLFW/HAL_GLFW_GL.cpp
+++ HAL/HAL/ExampleImplimentations/GLFW/HAL_GLFW_GL.cpp
@@ -231,7 +231,7 @@ HAL::texture_handle_t HAL::get_new_texture(const std::string_view fileName)
 void HAL::draw_texture(HAL::texture_handle_t texture, int32_t x, int32_t y, uint32_t w, uint32_t h)
 {
     TexturedQuad* t = (TexturedQuad*)texture;
-    t->draw();
+    t->draw(x, y, w, h);
 }
 
 void HAL::release_texture(texture_handle_t texture)
