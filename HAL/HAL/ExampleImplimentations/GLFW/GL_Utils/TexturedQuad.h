833dd128f829b984018e94b8575752ca7ed9e536
redhatdragon <paulabrams99@yahoo.com>
2023-12-14 05:10:58 -0500

    Update TexturedQuad.h
    This thing still doesn't work, was trying some stuff.

--- HAL/HAL/ExampleImplimentations/GLFW/GL_Utils/TexturedQuad.h
+++ HAL/HAL/ExampleImplimentations/GLFW/GL_Utils/TexturedQuad.h
@@ -24,10 +24,25 @@ struct TexturedQuad {
         { 0.5f,-0.5f, 1.0f, 0.0f },
         { 0.5f, 0.5f, 1.0f, 1.0f },
         {-0.5f, 0.5f, 0.0f, 1.0f }*/
-        {-0.5f, 0.5f, 0.0f, 0.0f},  //bottom left
-        {0.5f,  0.5f, 1.0f, 0.0f},  //bottom right
-        {0.5f, -0.5f, 1.0f, 1.0f},  //top right
-        {-0.5f, -0.5f, 0.0f, 1.0f}   //top left
+
+        //{-0.5f, 0.5f, 0.0f, 0.0f},  //bottom left
+        //{0.5f,  0.5f, 1.0f, 0.0f},  //bottom right
+        //{0.5f, -0.5f, 1.0f, 1.0f},  //top right
+        //{-0.5f, -0.5f, 0.0f, 1.0f}  //top left
+
+        //x, y, u, v
+        {0.5f, 0.5f, 1.0f, 1.0f},  //top right
+        {0.5f, -0.5f, 1.0f, 0.0f}, //bottom right
+        {-0.5f, -0.5f, 0.0f, 0.0f},//bottom left
+        {-0.5f,  0.5f, 0.0f, 1.0f} //top left
+    };
+    //static constexpr uint8_t indices[] = {
+    //    0, 1, 2,
+    //    2, 3, 0
+    //};
+    static constexpr uint8_t indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
     };
 
     //void init(const Vertex2D* verticies, const uint8_t* indicies, uint32_t vertCount, uint32_t indexCount);
@@ -55,27 +70,33 @@ struct TexturedQuad {
     texture.init("./data/textures/ChernoLogo.png");
 }*/
 void TexturedQuad::init(const char* filename, float x, float y, float w, float h) {
-    std::string dataDir = EE_getDirData();
-    shader.init(dataDir + "ShadersGL/TexturedQuad.shader");
-    //texture.init(dataDir + "textures/ChernoLogo.png");
-    texture.init(filename);
-    uint8_t indicies[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
     Vertex2D newVerts[4];
     for (uint32_t i = 0; i < 4; i++) {
         newVerts[i].x = verts[i].x * w; newVerts[i].y = verts[i].y * h;
         newVerts[i].x = verts[i].x + w / 2; newVerts[i].y = verts[i].y + h / 2;
         newVerts[i].x += x; newVerts[i].y += y;
         translateScreen2DToGL(newVerts[i].x, newVerts[i].y);
+        newVerts[i] = verts[i];
+        newVerts[i].u = verts[i].u;
+        newVerts[i].v = verts[i].v;
+        newVerts[i].log();
     }
     vertexBuffer.init(newVerts, 4 * sizeof(Vertex2D));
-    indexBuffer.init(indicies, 6, 1);
+    uint8_t newIndices[6] = {indices[0], indices[1], indices[2], indices[3]};
+    memcpy(newIndices, indices, 6);
+    for (uint32_t i = 0; i < 6; i++) {
+        HAL_LOG("{}\n", newIndices[i]);
+    }
+    indexBuffer.init(newIndices, 6, 1);
     vertexArray.init();
-    vertexLayout.push<float>(2);
-    vertexLayout.push<float>(2);
+    vertexLayout.push<float>(2);  // position
+    vertexLayout.push<float>(2);  // texCoord (based off Vector2D's u,v values)
     vertexArray.addBuffer(vertexBuffer, vertexLayout);
+
+    std::string dataDir = EE_getDirData();
+    shader.init(dataDir + "ShadersGL/TexturedQuad.shader");
+    //texture.init(dataDir + "textures/ChernoLogo.png");
+    texture.init(filename);
 }
 void TexturedQuad::destruct() {
     vertexBuffer.destruct();
@@ -91,6 +112,7 @@ void TexturedQuad::draw() {
     vertexBuffer.bind();
     indexBuffer.bind();
     texture.bind();
+    shader.bind();
     shader.setUniform4f("u_color", colors[0], colors[1], colors[2], colors[3]);
     shader.setUniform1i("u_texture", 0);
     //glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
@@ -98,20 +120,22 @@ void TexturedQuad::draw() {
     //translateScreen2DToGL(offsetX, offsetY);
     //glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
     //shader.setUniformMat4f("u_MVP", proj);
-    shader.bind();
+    ////shader.bind();
     vertexArray.bind();
 
-    glDrawElements(GL_TRIANGLES, indexBuffer.getCount(), GL_UNSIGNED_BYTE, nullptr);
+    vertexBuffer.bind();
+    indexBuffer.bind();
+    GL_CALL(glDrawElements(GL_TRIANGLES, indexBuffer.getCount(), GL_UNSIGNED_BYTE, nullptr));
 }
 void TexturedQuad::draw(float x, float y, float w, float h) {
     setPosAndSiz(x, y, w, h);
     draw();
 }
 void TexturedQuad::setPosAndSiz(float x, float y, float w, float h) {
-    uint8_t indicies[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
+    //uint8_t indices[] = {
+    //    0, 1, 2,
+    //    2, 3, 0
+    //};
     Vertex2D newVerts[4];
     for (uint32_t i = 0; i < 4; i++) {
         newVerts[i].u = verts[i].u; newVerts[i].v = verts[i].v;
@@ -122,8 +146,8 @@ void TexturedQuad::setPosAndSiz(float x, float y, float w, float h) {
     }
 
     vertexBuffer.buffer(newVerts, 4 * sizeof(Vertex2D));
-    indexBuffer.buffer(indicies, 6, 1);
+    indexBuffer.buffer(indices, 6, 1);
     vertexBuffer.bind();
     indexBuffer.bind();
-    vertexArray.addBuffer(vertexBuffer, vertexLayout);
+    //vertexArray.addBuffer(vertexBuffer, vertexLayout);
 }
