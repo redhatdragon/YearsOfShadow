344b12a88cca316ad69e503d6a0a4dda6fc73d75
redhatdragon <paulabrams99@yahoo.com>
2023-12-18 21:59:05 -0500

    Added spawnTest for network testing

--- YearsOfShadow/YearsOfShadow/Systems/SystemNetwork.h
+++ YearsOfShadow/YearsOfShadow/Systems/SystemNetwork.h
@@ -28,6 +28,7 @@ public:
 		OPTICK_EVENT();
 		#if GAME_TYPE == GAME_TYPE_SERVER
 			serverLogic();
+			spawnTest();
 		#elif GAME_TYPE == GAME_TYPE_CLIENT
 			clientLogic();
 		#endif
@@ -46,7 +47,7 @@ private:
 		entities.reserve(count);
 		for (uint32_t i = 0; i < count; i++) {
 			entities.push_back(ecs.getOwner(replicateEntityComponentID, i));
-}
+		}		
 		uint32_t size;
 		SerialEntity* seBuff = SerialEntity::constructSerialEntityBuffer(&entities[0], count, size);
 		nm.trySendTo("127.0.0.1", (uint8_t*)seBuff, size);
@@ -73,4 +74,23 @@ private:
 		}
 	}
 	#endif
+
+	void spawnTest() {
+		if (ecs.getTicksPassed() % 60)
+			return;
+		EntityID entity = ecs.getNewEntity();
+		uint32_t x = rand() % 200 + 20;
+		uint32_t y = rand() % 200 + 20;
+		uint32_t z = rand() % 100;
+		BodyID bodyID = physics.addBodyBox(x, y, z, 1, 1, 1, to_void_ptr(entity), true);
+		ecs.emplace(entity, bodyComponentID, &bodyID);
+		ReplicateEntity re;
+		re.init(entity, ecs.entityGetHandle(entity));
+		ecs.emplace(entity, replicateEntityComponentID, &re);
+		ComponentID instancedMeshComponentID = ecs.registerComponentAsBlittable("instancedMesh", sizeof(u32));
+		u32 imeshID = instancedMeshCodex.add("./Data/Meshes/Props/Dynamite.obj");
+		instancedMeshCodex.setTexture(imeshID, "./Data/Meshes/Props/D_Dynamite.png");
+		instancedMeshCodex.setSize(imeshID, {1, 1, 1});
+		ecs.emplace(entity, instancedMeshComponentID, &imeshID);
+	}
 };
