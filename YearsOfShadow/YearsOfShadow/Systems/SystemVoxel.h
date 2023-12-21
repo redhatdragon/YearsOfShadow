51e5b4b6f3cdfd4d2601ea9b4862fccc4196784d
redhatdragon <paulabrams99@yahoo.com>
2023-12-21 13:44:22 -0500

    Cleanup SystemVoxel

--- YearsOfShadow/YearsOfShadow/Systems/SystemVoxel.h
+++ YearsOfShadow/YearsOfShadow/Systems/SystemVoxel.h
@@ -4,10 +4,6 @@
 #include <iostream>
 #include <memory>
 
-//std::vector<int> testVec;
-//FlatBuffer<int, 100000000> testBuffer;
-//std::unique_ptr<int[]> testBuffer = std::make_unique<int[]>(100000000);
-
 class SystemVoxel : public System {
 	ComponentID controllerComponentID;
 public:
@@ -15,13 +11,6 @@ public:
         OPTICK_THREAD("MainThread");
         OPTICK_EVENT();
 		voxelWorld.init(100, 0, 0, 0);
-		//testBuffer.init(100000000);
-		//for (uint32_t i = 0; i < 100000000; i+=4) {
-		//	testBuffer[i] = i;
-		//	testBuffer[i+1] = i+1;
-		//	testBuffer[i+2] = i+2;
-		//	testBuffer[i+3] = i+3;
-		//}
         controllerComponentID = ecs.registerComponentAsBlittable("controller", sizeof(Controller));
 	}
 	virtual void run() {
@@ -29,7 +18,6 @@ public:
         OPTICK_EVENT();
 		Controller* controller = (Controller*)ecs.getComponentBuffer(controllerComponentID);
 		auto pos = controller->cam.getPosition();
-		//voxelWorld.display((uint32_t)pos.x, (uint32_t)pos.z);
 		voxelWorld.update((uint32_t)pos.x, (uint32_t)pos.z);
 	}
 	virtual void destruct() {
